d411ca581edd2b93a9138f4e5a196eebd1363a60
Jared Cacurak <jared.cacurak@gmail.com>
2013-11-14 15:56:17 -0500

    Rearrange the parameters in the function #countWhile.

--- problem-2/problem.js
+++ problem-2/problem.js
@@ -1,7 +1,7 @@
 var generation = [9, 10, 21, 20, 7, 11, 4, 15, 7, 7, 14, 5, 20, 6, 29, 8, 11, 19, 18, 22, 29, 14, 27, 17, 6, 22, 12, 18, 18, 30],
     overhead = [21, 16, 19, 26, 26, 7, 1, 8, 17, 14, 15, 25, 20, 3, 24, 5, 28, 9, 2, 14, 9, 25, 15, 13, 15, 9, 6, 20, 27, 22],
     budget = 2912;
- 
+
 maxNumberOfCards(budget, generation, overhead); // 17
 
 /**
@@ -15,16 +15,15 @@ maxNumberOfCards(budget, generation, overhead); // 17
 function maxNumberOfCards(budget, generation, overhead) {
     'use strict';
 
-    return countWhile(lessThan(budget), nextCard(generation, overhead));
+    return countWhile(nextCard(generation, overhead), lessThan(budget));
 
-
-    function countWhile(check, fn) {
+    function countWhile(fn, condition) {
         var count, value;
 
         count = -1;
         value = fn();
 
-        while (check(value)) {
+        while (condition(value)) {
             value = fn();
             count += 1;
         }
